bfad9bd9b8cff6c1148c838761802964eba52238
Pari Dhanakoti <parimala.dhanakoti@hatch.com>
2024-07-01 06:17:42 -0400

    main method setup

--- FreightManagement/FreightManagement/Program.cs
+++ FreightManagement/FreightManagement/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FreightManagement;
+public class Program
+{
+	private static void Main(string[] args)
+	{
+		PrintSeperator();
+		LoadFlightSchedule();
+		PrintSeperator();
+		LoadOrderSchedule();
+		PrintSeperator();
+	}
+
+	static void PrintSeperator() => Console.WriteLine("#################################################");
+
+	public static void LoadFlightSchedule()
+	{
+		Console.WriteLine("Flight Schedule");
+	}
+
+	public static void LoadOrderSchedule()
+	{
+		Console.WriteLine("Order Schedule");
+	}
+}
